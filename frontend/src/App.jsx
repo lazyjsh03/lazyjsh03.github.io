5f447116811a86329888dab1504d72bec48e998a
lazyJSH <lazy.jsh03@gmail.com>
2025-07-02 17:51:46 +0900

    feat: 라우팅 설정, Home 페이지 생성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+// pages
+import Home from "./pages/Home";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
